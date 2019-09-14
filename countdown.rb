651054dc48d9c5b418ee710d5eb50259221907c9
John Schmidley <tlg8n@outlook.com>
2019-09-14 21:48:47 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,7 +1,6 @@
 #write your code here
 
 def countdown(x)
-x = 10
 time = Time.now
 sleep 5
 while x > 0
@@ -12,5 +11,14 @@ return "HAPPY NEW YEAR!"
 end
 
 def countdown_with_sleep(x)
-
+def countdown(x)
+x = 10
+time = Time.now
+sleep 5
+while x > 0
+  puts "#{x} SECOND(S)!"
+  x = x-1
+end
+return "HAPPY NEW YEAR!"
+end
 
